4c8b1ea234b9b8712b9acc239a5fa665ac8066ae
ayvemake <anilcan.kahraman@gmail.com>
2024-12-25 20:08:29 +0100

    Sign uo form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/about-us", to: "about#index", as: :about
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "main#index"
+  get "/sign-up", to: "registrations#new"
+  post "/sign-up", to: "registrations#create"
 end
