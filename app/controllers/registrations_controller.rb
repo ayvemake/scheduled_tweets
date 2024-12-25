9cfff68d719c6bf124f1f31c757dd1084f1c4965
ayvemake <anilcan.kahraman@gmail.com>
2024-12-25 23:53:21 +0100

    Handling sign up errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,20 @@ class RegistrationsController < ApplicationController
     end
 
     def create
-        # Add your create logic here
+        @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation))
+        if @user.save
+            redirect_to root_path, notice: "Account created successfully"
+        else
+            flash[:alert] = "Something went wrong"
+            render :new, status: :unprocessable_entity #add this for compatibility with rails 7
+        end
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end 
+
+
 end 
