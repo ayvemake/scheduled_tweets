8bf1eecd5dd3912bba71eb36333407fcd490c45e
ayvemake <anilcan.kahraman@gmail.com>
2024-12-25 18:26:13 +0100

    URL Helpers and link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/about", to: "about#index"
+  get "/about-us", to: "about#index", as: :about
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "main#index"
 end
