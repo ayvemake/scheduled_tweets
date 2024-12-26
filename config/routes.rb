8fad4d7db07c3ea93b12686cf6c2cec82383f38d
ayvemake <anilcan.kahraman@gmail.com>
2024-12-26 18:17:59 +0100

    Password with mailer, confirmation messages and resets

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,14 @@ Rails.application.routes.draw do
   post "/sign-up", to: "registrations#create"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  get "password", to: "passwords#edit", as: :edit_password
-  patch "password", to: "passwords#update"
-  get "password/reset", to: "password_resets#new"
-  post "password/reset", to: "password_resets#create" 
-
-
   delete "/logout", to: "sessions#destroy"
   
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+  # Password reset routes
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/:token", to: "password_resets#edit", as: :edit_password_reset
+  patch "password/reset/:token", to: "password_resets#update"
 end
